ac0a21951a1dfa0c1d80795e51f47cd7dcfa3787
ivissivi <ivis31121999@gmail.com>
2022-03-17 22:37:19 +0200

    Added listview activity, manifests, location services

--- app/src/main/java/com/example/a2ndpractical/MapsActivity.java
+++ app/src/main/java/com/example/a2ndpractical/MapsActivity.java
@@ -1,12 +1,20 @@
 package com.example.a2ndpractical;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -14,14 +22,46 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.a2ndpractical.databinding.ActivityMapsBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    private FusedLocationProviderClient fusedLocationClient;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private boolean mPermissionDenied = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        if (ActivityCompat.checkSelfPermission(this.getApplicationContext(),
+                FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.checkSelfPermission(this.getApplicationContext(),
+                    COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+                fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+                fusedLocationClient.getLastLocation()
+                        .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                            @Override
+                            public void onSuccess(Location location) {
+                                // Got last known location. In some rare situations this can be null.
+                                if (location != null) {
+                                    // Logic to handle location object
+                                }
+                            }
+                        });
+            } else {
+                PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                        Manifest.permission.ACCESS_FINE_LOCATION, true);
+            }
+        } else {
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+        }
+
         super.onCreate(savedInstanceState);
 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
@@ -31,6 +71,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
     @Override
@@ -38,6 +79,18 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.repo:
+                Intent repoActivity = new Intent(this, RepositoryActivity.class);
+                startActivity(repoActivity);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     /**
@@ -52,10 +105,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
     }
 }
--- app/src/main/java/com/example/a2ndpractical/RepositoryActivity.java
+++ app/src/main/java/com/example/a2ndpractical/RepositoryActivity.java
@@ -1,4 +1,167 @@
 package com.example.a2ndpractical;
 
-public class RepositoryActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.squareup.picasso.Picasso;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class RepositoryActivity extends AppCompatActivity {
+
+    ArrayList<String> titles = new ArrayList<String>();
+    ArrayList<String> descriptions = new ArrayList<String>();
+    ArrayList<String> images = new ArrayList<String>();
+    ArrayList<String> urls = new ArrayList<String>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repository);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final MyListAdapter adapter = new MyListAdapter(this, titles, descriptions, images, urls);
+
+        ListView listView = (ListView) findViewById(R.id.item_list);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,  long id) {
+                String url = urls.get(position);
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+                Toast.makeText(RepositoryActivity.this, "on item click: " + urls.get(position), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://api.spaceflightnewsapi.net/v3/articles";
+        JsonArrayRequest jsonObjectRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+
+            @Override
+            public void onResponse(JSONArray response) {
+                Log.i("Praktiskais", "response:" + response.toString());
+                try {
+                    for (int i = 0; i < response.length(); i++) {
+                        JSONObject article = response.getJSONObject(i);
+                        titles.add(article.getString("title"));
+                        descriptions.add(article.getString("summary"));
+                        urls.add(article.getString("url"));
+                        images.add(article.getString("imageUrl"));
+
+                    }
+                } catch (Exception e) {
+                    Log.i("Praktiskais", "error:" + e.toString());
+                }
+
+                adapter.notifyDataSetChanged();
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i("Praktiskais", "error:" + error.toString());
+            }
+        });
+        queue.add(jsonObjectRequest);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.back_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.map:
+                finish();
+                Intent mapsActivity = new Intent(this, MapsActivity.class);
+                startActivity(mapsActivity);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private class MyListAdapter extends ArrayAdapter<String> {
+
+        private final Activity context;
+        private final ArrayList<String> maintitle;
+        private final ArrayList<String> description;
+        private final ArrayList<String> imgid;
+        private final ArrayList<String> url;
+
+        public MyListAdapter(Activity context, ArrayList<String> maintitle, ArrayList<String> description, ArrayList<String> imgid, ArrayList<String> url) {
+            super(context, R.layout.listview_item, maintitle);
+            // TODO Auto-generated constructor stub
+
+            this.context=context;
+            this.maintitle=maintitle;
+            this.description=description;
+            this.imgid=imgid;
+            this.url = url;
+
+        }
+
+        public View getView(int position, View view, ViewGroup parent) {
+            LayoutInflater inflater=context.getLayoutInflater();
+            View rowView=inflater.inflate(R.layout.listview_item, null,true);
+
+            TextView titleText = (TextView) rowView.findViewById(R.id.title);
+            ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
+            TextView subtitleText = (TextView) rowView.findViewById(R.id.description);
+
+            titleText.setText(maintitle.get(position));
+            Picasso.get().load(images.get(position)).into(imageView);
+//            imageView.setImageResource();
+            subtitleText.setText(description.get(position));
+
+            return rowView;
+
+        };
+    }
+
 }
